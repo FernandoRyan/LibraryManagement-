606f62dd4dda77599fb2e770c8bd6958e62095cc
Praneeth <praneethcc@gmail.com>
2020-07-18 11:42:11 +0530

    login signup

---
+++ Library Management System/Library Management System/Login.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Library_Management_System
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Praneeth\Documents\libraryDB.mdf;Integrated Security=True;Connect Timeout=30");
+                SqlDataAdapter sda = new SqlDataAdapter("select count(*) from login where email = '" + textBox1.Text + "' and password = '" + textBox2.Text + "'", conn);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                if (dt.Rows[0][0].ToString() == "1")
+                {
+                    this.Hide();
+                    las mm = new las();
+                    mm.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Please enter the correct username password", "alert", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Something is wrong in login");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Registraton registraton = new Registraton();
+            registraton.Show();
+        }
+    }
+}
