036ec1954074b7a2d3700a2b41fce3d46de3d926
RyanAnjelo <64610012+RyanAnjelo@users.noreply.github.com>
2020-07-28 22:29:52 +0530

    update of the db and some changes

--- Library Management System/Library Management System/Registraton.cs
+++ Library Management System/Library Management System/Registraton.cs
@@ -8,11 +8,17 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Runtime.InteropServices;
 
 namespace Library_Management_System
 {
     public partial class Registraton : Form
     {
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
         public Registraton()
         {
             InitializeComponent();
@@ -35,15 +41,15 @@ namespace Library_Management_System
                 }
                 else
                 {
-                    SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='E:\Library System\Login.mdf';Integrated Security=True;Connect Timeout=30");
+                    SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='E:\Library System\Register.mdf';Integrated Security=True;Connect Timeout=30");
                     conn.Open();
-                    String insert = "insert into LG (StudentID,Fname,Lname,email,contact,password) values ('" + SID.Text + "','" + Fnametxt.Text + "','" + Lnametxt.Text + "','" + emailtxt.Text + "','" + contacttxt.Text + "','" + pwtxt.Text + "')";
+                    String insert = "insert into LG (ID,Fname,Lname,email,contact,password) values ('" + SID.Text + "','" + Fnametxt.Text + "','" + Lnametxt.Text + "','" + emailtxt.Text + "','" + contacttxt.Text + "','" + pwtxt.Text + "')";
                     SqlCommand cmd = new SqlCommand(insert, conn);
                     cmd.ExecuteNonQuery();
                     this.Hide();
                     las home = new las();
                     home.Show();
-                    MessageBox.Show("Registration succesfull.!");
+                    MessageBox.Show("Registration succesfull!!");
                 }
 
             }
@@ -54,6 +60,30 @@ namespace Library_Management_System
 
 
 
+        }
+
+        private void iconButtonMin_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void iconButtonMax_Click(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Normal)
+                WindowState = FormWindowState.Maximized;
+            else
+                WindowState = FormWindowState.Normal;
+        }
+
+        private void iconButtonclose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Registraton_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
         }
     }
 }
