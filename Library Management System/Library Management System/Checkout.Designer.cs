982bc5c9f05fed2b104345fa11a86e608a8391fd
Praneeth <praneethcc@gmail.com>
2020-08-08 11:09:40 +0530

    Created checkout form

--- Library Management System/Library Management System/Checkout.Designer.cs
+++ Library Management System/Library Management System/Checkout.Designer.cs
@@ -29,11 +29,311 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label7 = new System.Windows.Forms.Label();
+            this.BtnSearch = new System.Windows.Forms.Button();
+            this.txtSid = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ContactLable = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.EmailLable = new System.Windows.Forms.Label();
+            this.LnameLable = new System.Windows.Forms.Label();
+            this.FnameLable = new System.Windows.Forms.Label();
+            this.LendDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BookName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.BookID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DatagridViewLend = new System.Windows.Forms.DataGridView();
+            this.StudentId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lendBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DatagridViewLend)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lendBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI Semibold", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label7.Location = new System.Drawing.Point(3, 8);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(247, 37);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Lend Books Details";
+            // 
+            // BtnSearch
+            // 
+            this.BtnSearch.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.BtnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnSearch.ForeColor = System.Drawing.Color.White;
+            this.BtnSearch.Location = new System.Drawing.Point(303, 28);
+            this.BtnSearch.Name = "BtnSearch";
+            this.BtnSearch.Size = new System.Drawing.Size(94, 31);
+            this.BtnSearch.TabIndex = 19;
+            this.BtnSearch.Text = "Search";
+            this.BtnSearch.UseVisualStyleBackColor = false;
+            this.BtnSearch.Click += new System.EventHandler(this.BtnSearch_Click);
+            // 
+            // txtSid
+            // 
+            this.txtSid.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSid.Location = new System.Drawing.Point(123, 28);
+            this.txtSid.Name = "txtSid";
+            this.txtSid.Size = new System.Drawing.Size(174, 31);
+            this.txtSid.TabIndex = 17;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(892, 50);
+            this.panel2.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label5.Location = new System.Drawing.Point(4, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(181, 32);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Student Details";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightCyan;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.ContactLable);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.EmailLable);
+            this.panel1.Controls.Add(this.LnameLable);
+            this.panel1.Controls.Add(this.FnameLable);
+            this.panel1.Location = new System.Drawing.Point(17, 74);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(892, 189);
+            this.panel1.TabIndex = 18;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(22, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "First Name: ";
+            // 
+            // ContactLable
+            // 
+            this.ContactLable.AutoSize = true;
+            this.ContactLable.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ContactLable.ForeColor = System.Drawing.Color.Black;
+            this.ContactLable.Location = new System.Drawing.Point(460, 125);
+            this.ContactLable.Name = "ContactLable";
+            this.ContactLable.Size = new System.Drawing.Size(66, 23);
+            this.ContactLable.TabIndex = 9;
+            this.ContactLable.Text = "ccccccc";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(22, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Last Name: ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.Black;
+            this.label9.Location = new System.Drawing.Point(375, 125);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(79, 23);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Contact: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(375, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Email: ";
+            // 
+            // EmailLable
+            // 
+            this.EmailLable.AutoSize = true;
+            this.EmailLable.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmailLable.ForeColor = System.Drawing.Color.Black;
+            this.EmailLable.Location = new System.Drawing.Point(460, 85);
+            this.EmailLable.Name = "EmailLable";
+            this.EmailLable.Size = new System.Drawing.Size(73, 23);
+            this.EmailLable.TabIndex = 7;
+            this.EmailLable.Text = "eeeeeee";
+            // 
+            // LnameLable
+            // 
+            this.LnameLable.AutoSize = true;
+            this.LnameLable.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LnameLable.ForeColor = System.Drawing.Color.Black;
+            this.LnameLable.Location = new System.Drawing.Point(130, 125);
+            this.LnameLable.Name = "LnameLable";
+            this.LnameLable.Size = new System.Drawing.Size(38, 23);
+            this.LnameLable.TabIndex = 6;
+            this.LnameLable.Text = "lllllll";
+            // 
+            // FnameLable
+            // 
+            this.FnameLable.AutoSize = true;
+            this.FnameLable.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FnameLable.ForeColor = System.Drawing.Color.Black;
+            this.FnameLable.Location = new System.Drawing.Point(130, 85);
+            this.FnameLable.Name = "FnameLable";
+            this.FnameLable.Size = new System.Drawing.Size(58, 23);
+            this.FnameLable.TabIndex = 5;
+            this.FnameLable.Text = "ffffffff";
+            // 
+            // LendDate
+            // 
+            this.LendDate.DataPropertyName = "LendDate";
+            this.LendDate.HeaderText = "LendDate";
+            this.LendDate.Name = "LendDate";
+            // 
+            // BookName
+            // 
+            this.BookName.DataPropertyName = "BookName";
+            this.BookName.HeaderText = "BookName";
+            this.BookName.Name = "BookName";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.panel3.Controls.Add(this.label7);
+            this.panel3.Location = new System.Drawing.Point(17, 263);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(892, 54);
+            this.panel3.TabIndex = 20;
+            // 
+            // BookID
+            // 
+            this.BookID.DataPropertyName = "BookID";
+            this.BookID.HeaderText = "BookID";
+            this.BookID.Name = "BookID";
+            // 
+            // DatagridViewLend
+            // 
+            this.DatagridViewLend.AutoGenerateColumns = false;
+            this.DatagridViewLend.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DatagridViewLend.BackgroundColor = System.Drawing.Color.LightCyan;
+            this.DatagridViewLend.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DatagridViewLend.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.StudentId,
+            this.BookID,
+            this.BookName,
+            this.LendDate});
+            this.DatagridViewLend.DataSource = this.lendBindingSource;
+            this.DatagridViewLend.Location = new System.Drawing.Point(17, 317);
+            this.DatagridViewLend.Name = "DatagridViewLend";
+            this.DatagridViewLend.RowHeadersVisible = false;
+            this.DatagridViewLend.RowTemplate.Height = 24;
+            this.DatagridViewLend.Size = new System.Drawing.Size(892, 232);
+            this.DatagridViewLend.TabIndex = 16;
+            // 
+            // StudentId
+            // 
+            this.StudentId.DataPropertyName = "StudentId";
+            this.StudentId.HeaderText = "StudentId";
+            this.StudentId.Name = "StudentId";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(16, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 23);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Student ID: ";
+            // 
+            // lendBindingSource
+            // 
+            this.lendBindingSource.DataMember = "Lend";
+            // 
+            // Checkout
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.PowderBlue;
+            this.ClientSize = new System.Drawing.Size(925, 576);
+            this.Controls.Add(this.BtnSearch);
+            this.Controls.Add(this.txtSid);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.DatagridViewLend);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Checkout";
             this.Text = "Checkout";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DatagridViewLend)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lendBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button BtnSearch;
+        private System.Windows.Forms.TextBox txtSid;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label ContactLable;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label EmailLable;
+        private System.Windows.Forms.Label LnameLable;
+        private System.Windows.Forms.Label FnameLable;
+        private System.Windows.Forms.BindingSource lendBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LendDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BookName;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BookID;
+        private System.Windows.Forms.DataGridView DatagridViewLend;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StudentId;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Library Management System/Library Management System/Checkout.cs
+++ Library Management System/Library Management System/Checkout.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,54 @@ namespace Library_Management_System
         public Checkout()
         {
             InitializeComponent();
+        }
+
+        //load student details by student ID
+        public void LoadStudentDetail()
+        {
+            String StudentId = txtSid.Text;
+
+            SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Praneeth\Desktop\New folder\Register.mdf;Integrated Security=True;Connect Timeout=30");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("Select Fname,Lname,email,contact from LG where ID = @Id", conn);
+            cmd.Parameters.AddWithValue("@Id", StudentId);
+            SqlDataReader da = cmd.ExecuteReader();
+            while (da.Read())
+            {
+                FnameLable.Text = da.GetValue(0).ToString();
+                LnameLable.Text = da.GetValue(1).ToString();
+                EmailLable.Text = da.GetValue(2).ToString();
+                ContactLable.Text = da.GetValue(3).ToString();
+            }
+            conn.Close();
+
+
+
+
+        }
+        //load the gridviwe by student ID
+        public void LoadGridviwe()
+        {
+            String StudentId = txtSid.Text;
+
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Praneeth\Desktop\New folder\Register.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlCommand cmd = new SqlCommand("select * from Lend where StudentId = @Id", con);
+            cmd.Parameters.AddWithValue("@Id", StudentId);
+            con.Open();
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            con.Close();
+            DatagridViewLend.DataSource = dt;
+        }
+
+        private void BtnSearch_Click(object sender, EventArgs e)
+        {
+            //calling LoadStudent Function
+            LoadStudentDetail();
+
+            //calling Loadgideviwe function
+            LoadGridviwe();
         }
     }
 }
